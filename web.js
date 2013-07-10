5365f8b28f41af21e66a8044780847ba1827ae60
Srikanth  Srungarapu <ssrungar@maymakeloft-lm.champ.corp.yahoo.com>
2013-07-10 12:08:51 -0500

    Making web.js read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('/etc/passwd', function (err, data) {
+	    if (err) throw err;
+	      response.send(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
