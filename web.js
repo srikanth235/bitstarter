e544b1b955b2114aad1ae4500ffeef78ac13e8d2
Srikanth  Srungarapu <ssrungar@maymakeloft-lm.champ.corp.yahoo.com>
2013-07-10 12:16:41 -0500

    Update web.js

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('/etc/passwd', function (err, data) {
+  fs.readFileSync('/index.html', function (err, data) {
 	    if (err) throw err;
-	      response.send(data);
+	    response.send(data);
   });
 });
 
