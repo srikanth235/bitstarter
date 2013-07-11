73dd29902eff796c572abcb93afbbe249ccd7743
Srikanth <srikanth@srikanth-Lenovo-Z580.(none)>
2013-07-11 00:07:19 -0500

    Updating web.js

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
+  fs.readFileSync('index.html', 'utf8', function (err, data) {
 	    if (err) throw err;
-	    response.send(data.toString('utf8'));
+	    response.send(data);
   });
 });
 
